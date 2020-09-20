788bcdc99fee44d202d5e16cec8d3b27e165b2d9
= <71273986+Sunirase@users.noreply.github.com>
2020-09-20 15:04:40 +0300

    css directory create

--- index.php
+++ index.php
@@ -6,29 +6,8 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
 
-<style>
 
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: #17a2b8;
-    height: 100vh;
-  }
-  #login .container #login-row #login-column #login-box {
-    margin-top: 120px;
-    max-width: 600px;
-    height: 320px;
-    border: 1px solid #9C9C9C;
-    background-color: #EAEAEA;
-  }
-  #login .container #login-row #login-column #login-box #login-form {
-    padding: 20px;
-  }
-  #login .container #login-row #login-column #login-box #login-form #register-link {
-    margin-top: -85px;
-  }
-
-</style>
+<link rel="stylesheet" href="css/main.css">
 
 
 <body>
